9cf6ea13516e2283bc7bf48e1f75bb13aa8bd64d
Ivo van der Marel <ivo.marel@gmail.com>
2020-12-03 17:06:46 -0800

    Added SimpleOAIBehavior Small update to the demo to provide more specific behaviors to Chad & Emma.

---
+++ Assets/OpenAI_Unity/OAI/Behaviors/SimpleOAIBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace OpenAI_Unity
+{
+    public class SimpleOAIBehavior : OAIBehavior
+    {
+        [TextArea(5,20)]
+        public string Description;
+
+        public override string GetAsText()
+        {
+            return Description;
+        }
+    }
+
+}
